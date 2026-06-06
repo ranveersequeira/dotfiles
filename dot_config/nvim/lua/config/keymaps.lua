cd78d50907b840c1e67c055f5a2315a45219ac57
Ranveer Kumar <ranveer.kumar@travenues.com>
2026-06-06 13:59:03 +0530

    fix: restore nvim comment keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,36 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
----Remap <leader>/ to toggle comments
--- "gcc" toggles the current line, "gc" toggles visual selection
--- { remap = true } is important so it triggers the plugin's "gcc" command
 local map = vim.keymap.set
 
-map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
-map("v", "<leader>/", "gc", { desc = "Toggle Comment", remap = true })
--- Map Grep to <leader>fg since <leader>/ is now taken
+local function toggle_line_comment()
+  require("Comment.api").toggle.linewise.current()
+end
+
+local function toggle_visual_comment()
+  local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+  vim.api.nvim_feedkeys(esc, "nx", false)
+  require("Comment.api").toggle.linewise(vim.fn.visualmode())
+end
+
+map("n", "<leader>/", toggle_line_comment, { desc = "Toggle Comment" })
+map("x", "<leader>/", toggle_visual_comment, { desc = "Toggle Comment" })
+
 map("n", "<leader>fg", LazyVim.pick("live_grep"), { desc = "Grep (Root Dir)" })
+
+map("n", "<leader>xd", vim.diagnostic.setloclist, { desc = "Buffer Diagnostics List" })
+map("n", "<leader>xq", vim.diagnostic.setqflist, { desc = "Workspace Diagnostics List" })
+map("n", "]e", function()
+  vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR })
+end, { desc = "Next Error" })
+map("n", "[e", function()
+  vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR })
+end, { desc = "Previous Error" })
+map("n", "]w", function()
+  vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.WARN })
+end, { desc = "Next Warning" })
+map("n", "[w", function()
+  vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.WARN })
+end, { desc = "Previous Warning" })
+
+map({ "n", "x" }, "<M-j>", "10j", { desc = "Move Down 10 Lines" })
+map({ "n", "x" }, "<M-k>", "10k", { desc = "Move Up 10 Lines" })
+map("n", "<M-d>", "<C-d>zz", { desc = "Page Down Centered" })
+map("n", "<M-u>", "<C-u>zz", { desc = "Page Up Centered" })
--- dot_config/nvim/lua/plugins/keymap-override.lua
+++ dot_config/nvim/lua/plugins/keymap-override.lua
@@ -1,13 +1,8 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
+  {
+    "folke/snacks.nvim",
     keys = {
-      -- Disable the default <leader>/ for grep
       { "<leader>/", false },
-
-      -- Add your new keymap for commenting
-      { "<leader>/", "gcc", mode = "n", desc = "Toggle Comment (Line)", remap = true },
-      { "<leader>/", "gc", mode = "v", desc = "Toggle Comment (Visual)", remap = true },
     },
   },
 }
